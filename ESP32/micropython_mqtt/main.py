b6689ee246d6b365294007dc1057af5492cef0a5
Niraj-Fonseka <fonseka.live@gmail.com>
2022-11-02 22:21:07 -0500

    adding micropython mqtt

---
+++ ESP32/micropython_mqtt/main.py
@@ -0,0 +1,29 @@
+import network
+from mqtt import MQTTClient 
+import machine 
+import time 
+ 
+def sub_cb(topic, msg): 
+   print(msg) 
+ 
+wlan = network.WLAN(network.STA_IF)
+wlan.active(True)
+
+wlan.connect("boba", "ilikeicecreammochi")
+
+ 
+while not wlan.isconnected():  
+    machine.idle() 
+print("Connected to Wifi\n") 
+ 
+client = MQTTClient("1234", "192.168.1.182",user="", password="", port=1883) 
+client.set_callback(sub_cb) 
+client.connect()
+client.subscribe(topic="esp/testtopic") 
+while True: 
+    print("Sending ON") 
+    client.publish(topic="esp/testtopic", msg="ON")
+    time.sleep(1) 
+    print("Sending OFF") 
+    client.publish(topic="esp/testtopic", msg="OFF")
+    time.sleep(1) 
