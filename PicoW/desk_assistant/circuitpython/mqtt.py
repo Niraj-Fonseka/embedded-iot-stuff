9750b5eb730b601437e78adda496fa4db54ca3e2
Niraj Fonseka <fonseka.live@gmail.com>
2023-02-13 22:40:19 -0600

    Update mqtt.py

--- PicoW/desk_assistant/circuitpython/mqtt.py
+++ PicoW/desk_assistant/circuitpython/mqtt.py
@@ -1,5 +1,3 @@
-# SPDX-FileCopyrightText: 2021 ladyada for Adafruit Industries
-# SPDX-License-Identifier: MIT
 
 import time
 import ssl
@@ -7,79 +5,36 @@ import socketpool
 import wifi
 import adafruit_minimqtt.adafruit_minimqtt as MQTT
 
-# Add a secrets.py to your filesystem that has a dictionary called secrets with "ssid" and
-# "password" keys with your WiFi credentials. DO NOT share that file or commit it into Git or other
-# source control.
-# pylint: disable=no-name-in-module,wrong-import-order
-
-
-# Set your Adafruit IO Username and Key in secrets.py
-# (visit io.adafruit.com if you need to create an account,
-# or if you need your Adafruit IO key.)
-wifi_name = "WIFI_NAME"
-wifi_password = "WIFI_PASSWORD"
+wifi_name = "boba"
+wifi_password = "ilikeicecreammochi"
 
 print("Connecting to wifi")
 wifi.radio.connect(wifi_name, wifi_password)
 print("Connected to wifi")
+
 ### Feeds ###
 
-# Setup a feed named 'photocell' for publishing to a feed
-
-# Setup a feed named 'onoff' for subscribing to changes
-onoff_feed = "niraj/feeds/onoff"
-
-### Code ###
-
-# Define callback methods which are called when events occur
-# pylint: disable=unused-argument, redefined-outer-name
-def connected(client, userdata, flags, rc):
-    # This function will be called when the client is connected
-    # successfully to the broker.
-    print("Connected! Listening for topic changes on %s" % onoff_feed)
-    # Subscribe to all changes on the onoff_feed.
-    client.subscribe(onoff_feed)
 
 
-def disconnected(client, userdata, rc):
-    # This method is called when the client is disconnected
-    print("Disconnected from Adafruit IO!")
+def InitializeMQTT(connected_callback,on_disconnect_callback,on_message_callback):
+    # Create a socket pool
+    pool = socketpool.SocketPool(wifi.radio)
 
+    # Set up a MiniMQTT Client
+    mqtt_client = MQTT.MQTT(
+        broker='192.168.1.152',
+        port=1883,
+        socket_pool=pool,
+        #ssl_context=ssl.create_default_context(),
+    )
 
-def message(client, topic, message):
-    # This method is called when a topic the client is subscribed to
-    # has a new message.
-    print("New message on topic {0}: {1}".format(topic, message))
+    # Setup the callback methods abovea
+    mqtt_client.on_connect = connected_callback
+    mqtt_client.on_disconnect = on_disconnect_callback
+    mqtt_client.on_message = on_message_callback
 
+    # Connect the client to the MQTT broker.
+    print("Connecting to Adafruit IO...")
+    mqtt_client.connect()
 
-# Create a socket pool
-pool = socketpool.SocketPool(wifi.radio)
-
-# Set up a MiniMQTT Client
-mqtt_client = MQTT.MQTT(
-    broker='192.168.1.152',
-    port=1883,
-    socket_pool=pool,
-    #ssl_context=ssl.create_default_context(),
-)
-
-# Setup the callback methods abovea
-mqtt_client.on_connect = connected
-mqtt_client.on_disconnect = disconnected
-mqtt_client.on_message = message
-
-# Connect the client to the MQTT broker.
-print("Connecting to Adafruit IO...")
-mqtt_client.connect()
-
-photocell_val = 0
-while True:
-    # Poll the message queue
-    mqtt_client.loop()
-
-    # Send a new message
-    print("Sending photocell value: %d..." % photocell_val)
-    mqtt_client.publish("circuitpython", photocell_val)
-    print("Sent!")
-    photocell_val += 1
-    time.sleep(5)
+    return mqtt_client
